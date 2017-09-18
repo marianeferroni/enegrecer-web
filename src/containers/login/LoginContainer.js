980aa08fc75e0e18408afdc968178d3c5ef41c25
Guilherme B.R <gbernard@thoughtworks.com>
2017-09-18 19:01:42 -0300

    Incluido redux login container

--- src/containers/login/LoginContainer.js
+++ src/containers/login/LoginContainer.js
@@ -1,10 +1,28 @@
 import React, { Component } from 'react';
 import Login from '../../components/login/Login';
+import { connect } from 'react-redux';
 
 class LoginContainer extends Component {
     render() {
-        return <div> <Login /> </div>;
+        return (
+        <div> 
+            <Login /> 
+            {
+                this.props.logged &&
+                <div> Login realizado com sucesso. </div>
+            }
+        </div>
+        );
     }
 }
 
-export default LoginContainer;
+const mapStateToProps = state => ({
+    logged: true,
+  });
+  
+  const reduxLogin = connect(
+    mapStateToProps,
+  )(LoginContainer);
+  
+  export default reduxLogin;
+  
